ac69c449176892f7efdc5f0d6fe81113c01bbb97
choihyuksoon <chs98412@gmail.com>
2023-02-02 09:26:55 +0900

    Update [완전탐색] 소수찾기 hyuksoon.py

--- Programmers - 고득점 Kit/[완전탐색] 소수찾기/hyuksoon.py
+++ Programmers - 고득점 Kit/[완전탐색] 소수찾기/hyuksoon.py
@@ -24,3 +24,23 @@ def solution(numbers):
             answer += 1
 
     return answer
+
+def solution(numbers):
+    answer = 0
+    data={}
+    def ck(x):
+        if x==0 or x==1:
+            return False
+        for i in range(2,int(math.sqrt(x))+1):
+            if x%i==0:
+                return False
+        return True
+    for i in range(1, len(numbers)+1):
+        temp = list(permutations(numbers, i))
+        for i in temp:
+            num=int(''.join(i))
+            if num not in data:
+                data[num]=1
+                if ck(num):
+                    answer+=1
+    return answer
