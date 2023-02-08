fec14d46d353b04852ae3f734c405392ae76d174
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2023-02-08 23:00:51 +0900

    Delete Programmers - 고득점 Kit/[해시] 완주하지 못한 선수 directory

--- Programmers - 고득점 Kit/[해시] 완주하지 못한 선수/kyuhyun.py
+++
@@ -1,8 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
