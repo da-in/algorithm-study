e7df4c670ced24975c472532a438f4dfd575c604
da-in <talown@naver.com>
2023-02-09 21:57:04 +0900

    :memo: Update [정렬] H-Index dain.py

--- Programmers - 고득점 Kit/[정렬] H-Index/dain.py
+++ Programmers - 고득점 Kit/[정렬] H-Index/dain.py
@@ -1,22 +1,4 @@
 def solution(citations):
-    answer = 0
-    length = max(citations) + 1
-    count = [0] * (length)
-    for c in citations:
-        count[c] += 1
-
-    for i in range(length - 1, 0, -1):
-        count[i - 1] += count[i]
-
-    for i in range(length):
-        if count[i] >= i:
-            answer = i
-
-    return answer
-
-
-# 개선한 코드
-def solution(citations):
     max_c = max(citations)
     count = [0] * (max_c + 1)
     for c in citations:
