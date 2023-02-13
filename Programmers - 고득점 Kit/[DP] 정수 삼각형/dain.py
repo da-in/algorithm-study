57c6e7467ffce5af7dd973fd2a68978d56c247ad
da-in <talown@naver.com>
2023-02-13 23:50:05 +0900

    :memo: Create [DP] 정수 삼각형 dain.py

---
+++ Programmers - 고득점 Kit/[DP] 정수 삼각형/dain.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(i + 1):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+    return max(triangle[-1])
