dc5fd043985a95560ed297f9b740ae660a0c0d32
da-in <talown@naver.com>
2023-02-14 17:49:34 +0900

    :memo: Update [그리디] 구명보트 dain.js

--- Programmers - 고득점 Kit/[그리디] 구명보트/dain.js
+++ Programmers - 고득점 Kit/[그리디] 구명보트/dain.js
@@ -1,13 +1,11 @@
 function solution(people, limit) {
   var answer = 0;
-  let a = 0;
-  let i = people.length - 1;
   people.sort((a, b) => a - b);
   while (people.length > 0) {
     answer += 1;
-    a = people.pop();
+    let a = people.pop();
     if (limit - a >= people[0]) {
-      for (i; i >= 0; i--) {
+      for (let i = 0; i < people.length; i++) {
         if (people[i] <= limit - a) {
           people.splice(i, 1);
           break;
