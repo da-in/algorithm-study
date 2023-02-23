7b11ecac50972e0eb0343ac6ea0f6bdf9a169031
chs0412 <52202546+chs0412@users.noreply.github.com>
2023-02-23 18:07:08 +0900

    Update [그리디] 섬 연결하기 hyuksoon.py

--- Programmers - 고득점 Kit/[그리디] 섬 연결하기/hyuksoon.py
+++ Programmers - 고득점 Kit/[그리디] 섬 연결하기/hyuksoon.py
@@ -8,12 +8,8 @@ def solution(n, costs):
     visit[costs[0][0]]=0
     visit[costs[0][1]]=0
     for i in costs[1:]:
-        print(data)
-        print(visit)
-        print("")
         if visit[i[0]] !=-1 and visit[i[1]] !=-1 and visit[i[0]]!=visit[i[1]] :
             answer+=i[2]
-            print(max(visit[i[0]],visit[i[1]]))
             maxnum=max(visit[i[0]],visit[i[1]])
             minnun=min(visit[i[0]],visit[i[1]])
             for i in data[maxnum]:
@@ -32,13 +28,8 @@ def solution(n, costs):
             visit[i[0]]=len(data)
             visit[i[1]]=len(data)
             data.append([i[0],i[1]])
-            
-        else:
-            print(i)
+
             
             
         
-    print(data)
     return answer
-
-print(solution(4,[[0,1,3],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
