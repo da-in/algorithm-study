739fd101462cbb84b240bab8d160b09198e87e8b
chs0412 <52202546+chs0412@users.noreply.github.com>
2023-03-02 10:08:30 +0900

    Update [그래프] 방의 개수 hyuksoon.py

--- Programmers - 고득점 Kit/[그래프] 방의 개수/hyuksoon.py
+++ Programmers - 고득점 Kit/[그래프] 방의 개수/hyuksoon.py
@@ -4,38 +4,38 @@ def solution(arrows):
     dy=[-1,-1,0,1,1,1,0,-1]
     dx=[0,1,1,1,0,-1,-1,-1]
     y,x=0,0
-    hash["00"]=[]
+    hash[(0,0)]=[]
     for i in arrows:
         ny=y+dy[i]
         nx=x+dx[i]
         nx2=x+(dx[i]/2)
         ny2=y+(dy[i]/2)
             
-        if str(ny)+str(nx) in hash:
-            if str(y)+str(x) not in hash[str(ny)+str(nx)]:
+        if (ny,nx) in hash:
+            if (y,x) not in hash[(ny,nx)]:
                 answer+=1
-                hash[str(ny)+str(nx)].append(str(y)+str(x))
-                hash[str(y)+str(x)].append(str(ny)+str(nx))
+                hash[(ny,nx)].append((y,x))
+                hash[(y,x)].append((ny,nx))
         else:
-            hash[str(ny)+str(nx)]=[]
-            hash[str(ny)+str(nx)].append(str(y)+str(x))
-            hash[str(y)+str(x)].append(str(ny)+str(nx))
+            hash[(ny,nx)]=[]
+            hash[(ny,nx)].append((y,x))
+            hash[(y,x)].append((ny,nx))
         if dy[i]!=0 and dx[i]!=0 :
-            if str(ny2)+str(nx2) in hash:
-                if str(y)+str(x) not in hash[str(ny2)+str(nx2)]:
+            if (ny2,nx2) in hash:
+                if (y,x) not in hash[(ny2,nx2)]:
                     answer+=1
-                    hash[str(ny2)+str(nx2)].append(str(y)+str(x))
-                    hash[str(ny2)+str(nx2)].append(str(ny)+str(nx))
+                    hash[(ny2,nx2)].append((y,x))
+                    hash[(ny2,nx2)].append((ny,nx))
                     
-                    hash[str(y)+str(x)].append(str(ny2)+str(nx2))
-                    hash[str(ny)+str(nx)].append(str(ny2)+str(nx2))
+                    hash[(y,x)].append((ny2,nx2))
+                    hash[(ny,nx)].append((ny2,nx2))
             else:
-                hash[str(ny2)+str(nx2)]=[]
-                hash[str(ny2)+str(nx2)].append(str(y)+str(x))
-                hash[str(ny2)+str(nx2)].append(str(ny)+str(nx))
+                hash[(ny2,nx2)]=[]
+                hash[(ny2,nx2)].append((y,x))
+                hash[(ny2,nx2)].append((ny,nx))
                 
-                hash[str(y)+str(x)].append(str(ny2)+str(nx2))
-                hash[str(ny)+str(nx)].append(str(ny2)+str(nx2))
+                hash[(y,x)].append((ny2,nx2))
+                hash[(ny,nx)].append((ny2,nx2))
 
 
         
