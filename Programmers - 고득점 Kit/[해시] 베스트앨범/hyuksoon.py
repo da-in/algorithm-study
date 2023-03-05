4a59de755ca6419af8cfbee1d8c3b37039d7830f
chs0412 <52202546+chs0412@users.noreply.github.com>
2023-03-05 16:17:28 +0900

    Create [해시] 베스트앨범 hyu ksoon.py

--- Programmers - 고득점 Kit/[해시] 베스트앨범/hyuksoon.py
+++ Programmers - 고득점 Kit/[해시] 베스트앨범/hyuksoon.py
@@ -1,3 +1,27 @@
 def solution(genres, plays):
     answer = []
+    h={}
+    musics={}
+    
+    for i,p in enumerate(genres):
+        if p in h:
+            h[p]+=plays[i]
+        else:
+            h[p]=plays[i]
+        
+        if p in musics:
+            musics[p].append([i,plays[i]])
+        else:
+            musics[p]=[[i,plays[i]]]
+            
+    for i in musics:
+        musics[i].sort(key=lambda x: (-x[1],x[0]))
+        
+    h=sorted(h.items(),key = lambda x: -x[1])
+    
+    for i in h:
+        answer.append(musics[i[0]][0][0])
+        if len(musics[i[0]])>1:
+            answer.append(musics[i[0]][1][0])
+        
     return answer
