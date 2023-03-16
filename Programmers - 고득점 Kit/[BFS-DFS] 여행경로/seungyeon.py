b088a298a1a1e9818947ab647448d56266e1ca26
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-03-16 17:23:13 +0900

    Create [BFS-DFS] 여행경로 seungyeon.py

---
+++ Programmers - 고득점 Kit/[BFS-DFS] 여행경로/seungyeon.py
@@ -0,0 +1,30 @@
+import collections
+answer = []
+graph = collections.defaultdict(list)
+visited = collections.defaultdict(list)
+
+def dfs(s, cnt, list, l):
+    if cnt == l:
+        answer.append(list)
+        return
+    
+    if len(answer) >= 1:
+        return
+
+    for a in range(len(graph[s])):
+        if visited[s][a] == 0:
+            visited[s][a] = 1
+            dfs(graph[s][a], cnt+1, list+[graph[s][a]], l)
+            visited[s][a] = 0
+
+def solution(tickets):
+    
+    for a,b in tickets:
+        graph[a].append(b)
+        visited[a].append(0)
+    for a, b in graph.items():
+        graph[a].sort() ## sort
+
+    dfs("ICN",0, ["ICN"], len(tickets))
+
+    return answer[:][0]
