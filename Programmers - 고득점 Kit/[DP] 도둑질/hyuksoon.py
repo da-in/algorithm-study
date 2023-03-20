003a573d94e1318c1cd1f325bdc34d326e99b5bc
chs98412 <chs98412@gmail.com>
2023-03-20 17:07:22 +0900

    Create [DP] 도둑질 hyuksoon.py

---
+++ Programmers - 고득점 Kit/[DP] 도둑질/hyuksoon.py
@@ -0,0 +1,34 @@
+def solution(money):
+    answer = 0
+    dp=[[0,0,0]for i in range(len(money))]
+    dp[0][0]=0
+    dp[0][1]=0
+    dp[0][2]=0
+    
+    for i in range(1,len(money)):
+        dp[i][0]=max(dp[i-1][1],dp[i-1][2])+money[i]
+        dp[i][1]=dp[i-1][0]
+        dp[i][2]=dp[i-1][1]
+    answer=max(dp[-1])
+        
+    
+    #끊어져 있을 때의 코드
+    dp=[[0,0,0]for i in range(len(money))]
+    dp[0][0]=money[0]
+    dp[0][1]=0
+    dp[0][2]=0
+    
+    for i in range(1,len(money)):
+        dp[i][0]=max(dp[i-1][1],dp[i-1][2])+money[i]
+        dp[i][1]=dp[i-1][0]
+        dp[i][2]=dp[i-1][1]
+    #answer=max(dp[-1])
+    answer=max(dp[-1][1],dp[-1][2],answer)
+    
+    return answer
+
+
+
+
+
+
