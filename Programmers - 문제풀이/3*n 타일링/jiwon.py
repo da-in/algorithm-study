f5547be61b1cbb59b8b936804b994ba659a2a37a
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-04-04 00:05:07 +0900

    create [3*n 타일링] jiwon.py

---
+++ Programmers - 문제풀이/3*n 타일링/jiwon.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    # 재료: 가로 2, 세로 1
+    # 목표: 가로 n, 세로 3
+    # 가로, 세로 배치 가능
+    i=n//2
+    x=1000000007
+    if n%2==0:
+        for a in range(len(x+1)):
+            b**(x-a)*(x-a+1)
+        return answer%x
+    else:
+        return 0
