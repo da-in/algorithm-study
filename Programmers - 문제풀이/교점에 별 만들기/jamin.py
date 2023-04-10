5873e5a9535dda2425b5f0a2759d2e069876fa77
jaminleee <jamin_00@naver.com>
2023-04-10 23:57:24 +0900

    Update [교점에 별 만들기] jamin.py

--- Programmers - 문제풀이/교점에 별 만들기/jamin.py
+++ Programmers - 문제풀이/교점에 별 만들기/jamin.py
@@ -1,8 +1,16 @@
+
+
+import itertools
 def solution(line):
     answer = []
     points = []
     integers = []
     
+    
+    #print(ncr)
+    # for i in ncr:
+    #     x = (i[0][1]
+    #     points.append([])
     
     for i in range(len(line)):
         for j in range(i+1, len(line)):
@@ -32,21 +40,25 @@ def solution(line):
     col_max = max(list(zip(*integers))[1])
     col_min= min(list(zip(*integers))[1])
             
-    arr = [["." for j in range(int(col_max-col_min)+1)] for i in range(int(row_max-row_min)+1)]
-    
-    
+    arr = [["." for j in range(int(row_max-row_min)+1)] for i in range(int(col_max-col_min)+1)]
 
-    print(arr[0])
+    
+    # for i in range(len(arr)):
+    #     for j in range(len(arr[i])):
+    #         print(arr[i][j])
+    # print(integers)
+    
     for i in integers:
         x = i[0] - row_min
         y = col_max - i[1]
-        print(y,x)
-        #arr[y][x] = '*'
+        # i = [y,x]
+        # print(i)
+        arr[y][x] = '*'
 
     
     for i,j in enumerate(arr):
         answer.append(''.join(j))
-        print(i,arr[i])
-    print(answer)
+    #     print(i,arr[i])
+    # print(answer)
         
-    return []
+    return answer
