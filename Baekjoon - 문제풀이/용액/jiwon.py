1d86b92a144bca33c0398eca3469350c34b0a13e
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-08-21 22:27:14 +0900

    Update jiwon.py

--- Baekjoon - 문제풀이/용액/jiwon.py
+++ Baekjoon - 문제풀이/용액/jiwon.py
@@ -1,47 +1,29 @@
 import sys
 
-N = input()
+N = int(input())
 solution_list = list(map(int, sys.stdin.readline().split()))
-MAX = 1000000000
-MIN = -1000000000
+sum = 2000000000
 
-sum0 = 2*MAX+1
-s1,s2 = 0,0
+for i in range(N):
+    current = solution_list[i]
 
-minus1, minus2 = MIN-1, MIN-1
-plus1, plus2 = MAX+1, MAX+1
-index = -1
-for i in range(len(solution_list)-1):
-    if solution_list[i]>0:
-        index = i
-        break
-    if solution_list[i]<0 and solution_list[i+1]<0:
-        minus1, minus2 = solution_list[i], solution_list[i+1]
-if index<len(solution_list)-1:
-    plus1,plus2 = solution_list[index], solution_list[index+1]
+    start = i+1
+    end = N-1
 
-# case1: 음수 중 작은거 2개
-if minus1!=MIN-1 and minus2!=MIN-1:
-    sum0 = -(minus1+minus2)
-    s1,s2 = minus1, minus2
+    while start<=end:
+        mid = (start+end)//2
+        temp_sum = current + solution_list[mid]
 
-# case2: 양수 중 작은거 2개
-if plus1!=MAX+1 and plus2!=MAX+1:
-    if plus1+plus2<sum0:
-        sum0 = plus1+plus2
-        s1, s2 = plus1, plus2
+        if abs(temp_sum) < sum:
+            sum = abs(temp_sum)
+            result = (solution_list[i], solution_list[mid])
 
-# case3: 음수+양수
-if index<len(solution_list)-1:
-    for i in solution_list[:index]:
-        for j in solution_list[index:]:
-            sum_solution = i+j
-            if sum_solution<0:
-                sum_solution = -sum_solution
-            if sum_solution<sum0:
-                sum0 = sum_solution
-                s1,s2 = i,j
+            if result == 0:
+                break
 
-print(s1, s2)
+        if temp_sum < 0:
+            start = mid+1
+        else:
+            end = mid-1
 
-# 음수 중 작은거 2개, 양수 중 작은거 2개, 음수+양수
+print(result[0], result[1])
