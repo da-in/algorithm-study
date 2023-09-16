79f616c26d31b4a623786fe9f7778ce1909baeda
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-09-17 01:59:44 +0900

    #317 Create [동전 1]jiwon.py

--- Baekjoon - 문제풀이/동전 1/jiwon.py
+++ Baekjoon - 문제풀이/동전 1/jiwon.py
@@ -3,4 +3,11 @@ from collections import deque
 
 n, k = map(int, sys.stdin.readline().split())
 coins = [int(sys.stdin.readline()) for _ in range(n)]
+dp = [0 for _ in range(k+1)]
+dp[0] = 1
 
+for coin in coins:
+    for i in range(coin, len(dp)):
+        dp[i] = dp[i] + dp[i-coin]
+
+print(dp[k])
